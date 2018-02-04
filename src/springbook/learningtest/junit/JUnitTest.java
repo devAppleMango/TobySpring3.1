e3f81893bea94ae2a4751b6725505380dfab9135
devapplemango <devapplemango@gmail.com>
2018-02-04 19:05:09 +0900

    2-6 addAngGet JUnit test

---
+++ src/springbook/learningtest/junit/JUnitTest.java
@@ -0,0 +1,9 @@
+package springbook.learningtest.junit;
+
+import org.junit.runner.JUnitCore;
+
+public class JUnitTest {
+	public static void main(String[] args) {
+		JUnitCore.main("springbook.user.test.UserDaoTest");
+	}
+}
