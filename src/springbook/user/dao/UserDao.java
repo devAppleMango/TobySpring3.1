909d0f7d22b2520be93886f9e45f1d655c525a68
devapplemango <devapplemango@gmail.com>
2018-02-06 23:39:17 +0900

    3-25 remove JdbcContext bean

--- src/springbook/user/dao/UserDao.java
+++ src/springbook/user/dao/UserDao.java
@@ -15,11 +15,9 @@ public class UserDao {
 	private DataSource dataSource;
 	private JdbcContext jdbcContext;	
 
-	public void setJdbcContext(JdbcContext jdbcContext) {
-		this.jdbcContext = jdbcContext;
-	}
-
 	public void setDataSource(DataSource dataSource) {
+		this.jdbcContext = new JdbcContext();
+		this.jdbcContext.setDataSource(dataSource);
 		this.dataSource = dataSource;
 	}
 
