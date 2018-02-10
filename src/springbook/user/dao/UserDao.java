f8f0ba1fe0f1329e2b1b3b70496e5fbc623bb5f7
devapplemango <devapplemango@gmail.com>
2018-02-11 01:00:42 +0900

    3-58 isolate RowMapper of User

--- src/springbook/user/dao/UserDao.java
+++ src/springbook/user/dao/UserDao.java
@@ -13,10 +13,21 @@ import springbook.user.domain.User;
 
 public class UserDao {
 	private JdbcTemplate jdbcTemplate;
+	
+	private RowMapper<User> userMapper = new RowMapper<User>() {
+		public User mapRow(ResultSet rs, int rowNum) throws SQLException {
+			User user = new User();
+			user.setId(rs.getString("id"));
+			user.setName(rs.getString("name"));
+			user.setPassword(rs.getString("password"));
+			return user;
+		}
+	};
 
 	public void setDataSource(DataSource dataSource) {
 		this.jdbcTemplate = new JdbcTemplate(dataSource);
 	}
+	
 
 	public void add(final User user) {
 		this.jdbcTemplate.update("insert into users(id, name, password) values(?,?,?)",
@@ -24,16 +35,7 @@ public class UserDao {
 	}
 
 	public User get(String id) throws ClassNotFoundException, SQLException {
-		return this.jdbcTemplate.queryForObject("select * from users where id  = ?", new Object[] {id},
-			new RowMapper<User>() {
-				public User mapRow(ResultSet rs, int rowNum) throws SQLException {
-					User user = new User();
-					user.setId(rs.getString("id"));
-					user.setName(rs.getString("name"));
-					user.setPassword(rs.getString("password"));
-					return user;
-				}
-		});
+		return this.jdbcTemplate.queryForObject("select * from users where id  = ?", new Object[] {id}, userMapper);
 	}
 
 	public void deleteAll() {
@@ -45,15 +47,6 @@ public class UserDao {
 	}
 	
 	public List<User> getAll() {
-		return this.jdbcTemplate.query("select * from users order by id",
-				new RowMapper<User>() {
-			public User mapRow(ResultSet rs, int rowNum) throws SQLException {
-				User user = new User();
-				user.setId(rs.getString("id"));
-				user.setName(rs.getString("name"));
-				user.setPassword(rs.getString("password"));
-				return user;
-			}
-		});
+		return this.jdbcTemplate.query("select * from users order by id", userMapper);
 	}
 }
