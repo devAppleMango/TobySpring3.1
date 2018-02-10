0c08bdebfc94a96f05e63d9e84bbd2ecddb0242b
devapplemango <devapplemango@gmail.com>
2018-02-11 00:20:17 +0900

    3-40 lineReadTemplate

--- src/springbook/learningtest/template/Calculator.java
+++ src/springbook/learningtest/template/Calculator.java
@@ -24,31 +24,44 @@ public class Calculator {
 		}
 	}
 	
+	public Integer lineReadTemplate(String filepath, LineCallback callback, int initVal) throws IOException {
+		BufferedReader br = null;
+		try {
+			br = new BufferedReader(new FileReader(filepath));
+			Integer res = initVal;
+			String line = null;
+			while((line = br.readLine()) != null) {
+				res = callback.doSomethingWithLine(line, res);
+			}
+			return res;
+		}
+		catch (IOException e) {
+			System.out.println(e.getMessage());
+			throw e;
+		}
+		finally {
+			if(br != null) {
+				try { br.close(); }
+				catch (IOException e) {System.out.println(e.getMessage());}
+			}
+		}
+	}
+	
 	public Integer calcSum(String  filepath) throws IOException {
-		BufferedReaderCallback sumCallback = new BufferedReaderCallback() {			
-			public Integer doSomethingWithReader(BufferedReader br) throws IOException {
-				Integer sum = 0;
-				String line = null;
-				while((line = br.readLine()) != null) {
-					sum += Integer.valueOf(line);
-				}
-				return sum;
+		LineCallback sumCallback = new LineCallback() {			
+			public Integer doSomethingWithLine(String line, Integer value) {				
+				return value + Integer.valueOf(line);
 			}
 		};
-		return fileReadTemplate(filepath, sumCallback);
+		return lineReadTemplate(filepath, sumCallback, 0);
 	}
 
 	public Integer calcMultiply(String filepath) throws IOException {
-		BufferedReaderCallback multiplyCallback = new BufferedReaderCallback() {			
-			public Integer doSomethingWithReader(BufferedReader br) throws IOException {
-				Integer sum = 1;
-				String line = null;
-				while((line = br.readLine()) != null) {
-					sum *= Integer.valueOf(line);
-				}
-				return sum;
+		LineCallback multiplyCallback = new LineCallback() {			
+			public Integer doSomethingWithLine(String line, Integer value) {				
+				return value * Integer.valueOf(line);
 			}
 		};
-		return fileReadTemplate(filepath, multiplyCallback);
+		return lineReadTemplate(filepath, multiplyCallback, 1);
 	}
 }
