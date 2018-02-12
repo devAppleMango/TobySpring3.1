0ce6bb534fd5b1a20440fac5b51ce3bbac49fc6c
devapplemango <devapplemango@gmail.com>
2018-02-12 23:12:01 +0900

    6-24 dynamic proxy

--- src/springbook/learningtest/jdk/ProxyTest.java
+++ src/springbook/learningtest/jdk/ProxyTest.java
@@ -3,6 +3,8 @@ package springbook.learningtest.jdk;
 import static org.hamcrest.CoreMatchers.is;
 import static org.junit.Assert.assertThat;
 
+import java.lang.reflect.Proxy;
+
 import org.junit.Test;
 
 public class ProxyTest {
@@ -21,5 +23,16 @@ public class ProxyTest {
 		assertThat(hello.sayHello("Toby"), is("HELLO TOBY"));
 		assertThat(hello.sayHi("Toby"), is("HI TOBY"));
 		assertThat(hello.sayThankYou("Toby"), is("THANK YOU TOBY"));
+	}
+	
+	@Test
+	public void dynamicUppercaseProxy() {
+		Hello proxiedHello = (Hello)Proxy.newProxyInstance(
+			getClass().getClassLoader(),
+			new Class[] {Hello.class},
+			new UppercaseHandler(new HelloTarget()));
+		assertThat(proxiedHello.sayHello("Toby"), is("HELLO TOBY"));
+		assertThat(proxiedHello.sayHi("Toby"), is("HI TOBY"));
+		assertThat(proxiedHello.sayThankYou("Toby"), is("THANK YOU TOBY"));
 	}
 }
