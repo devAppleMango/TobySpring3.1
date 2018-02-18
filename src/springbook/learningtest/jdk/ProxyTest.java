00c478fa2dc70bb2830723998cbb6570dd41b760
devapplemango <devapplemango@gmail.com>
2018-02-18 17:23:38 +0900

    6-41 ProxyFactoryBean learning test

--- src/springbook/learningtest/jdk/ProxyTest.java
+++ src/springbook/learningtest/jdk/ProxyTest.java
@@ -5,7 +5,10 @@ import static org.junit.Assert.assertThat;
 
 import java.lang.reflect.Proxy;
 
+import org.aopalliance.intercept.MethodInterceptor;
+import org.aopalliance.intercept.MethodInvocation;
 import org.junit.Test;
+import org.springframework.aop.framework.ProxyFactoryBean;
 
 public class ProxyTest {
 	
@@ -35,4 +38,67 @@ public class ProxyTest {
 		assertThat(proxiedHello.sayHi("Toby"), is("HI TOBY"));
 		assertThat(proxiedHello.sayThankYou("Toby"), is("THANK YOU TOBY"));
 	}
+	
+	@Test
+	public void proxyFactoryBean() {
+		ProxyFactoryBean pfBean = new ProxyFactoryBean();
+		pfBean.setTarget(new HelloTarget());
+		pfBean.addAdvice(new UppercaseAdvice());
+		
+		Hello proxiedHello = (Hello) pfBean.getObject();
+		assertThat(proxiedHello.sayHello("Toby"), is("HELLO TOBY"));
+		assertThat(proxiedHello.sayHi("Toby"), is("HI TOBY"));
+		assertThat(proxiedHello.sayThankYou("Toby"), is("THANK YOU TOBY"));
+	}
+	
+	static class UppercaseAdvice implements MethodInterceptor {
+		public Object invoke(MethodInvocation invocation) throws Throwable {
+			String ret = (String)invocation.proceed();
+			return ret.toUpperCase();
+		}
+	}
+	
+	static interface Hello {
+		String sayHello(String name);
+		String sayHi(String name);
+		String sayThankYou(String name);
+	}
+
+	static class HelloTarget implements Hello {
+
+		public String sayHello(String name) {
+			return "Hello " + name;
+		}
+
+		public String sayHi(String name) {
+			return "Hi " + name;
+		}
+
+		public String sayThankYou(String name) {
+			return "Thank You " + name;
+		}
+
+	}
+
+	static class HelloUppercase implements Hello {
+		Hello hello;
+
+		public HelloUppercase(Hello hello) {
+			this.hello = hello;
+		}
+		
+		public String sayHello(String name) {
+			return hello.sayHello(name).toUpperCase();
+		}
+
+		public String sayHi(String name) {
+			return hello.sayHi(name).toUpperCase();
+		}
+
+		public String sayThankYou(String name) {
+			return hello.sayThankYou(name).toUpperCase();
+		}
+
+	}
+
 }
