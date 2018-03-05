062e15275ff517f42878089049768acbba1a89bb
devapplemango <devapplemango@gmail.com>
2018-03-05 22:48:36 +0900

    7-36 sqlReader and sqlRegistry

--- src/springbook/user/service/NameMatchClassMethodPointcut.java
+++ src/springbook/user/service/NameMatchClassMethodPointcut.java
@@ -4,6 +4,7 @@ import org.springframework.aop.ClassFilter;
 import org.springframework.aop.support.NameMatchMethodPointcut;
 import org.springframework.util.PatternMatchUtils;
 
+@SuppressWarnings("serial")
 public class NameMatchClassMethodPointcut extends NameMatchMethodPointcut {
 	public void setMappedClassName(String mappedClassName) {
 		this.setClassFilter(new SimpleClassFilter(mappedClassName));
--- src/springbook/user/sqlservice/XmlSqlService.java
+++ src/springbook/user/sqlservice/XmlSqlService.java
@@ -13,12 +13,38 @@ import springbook.user.dao.UserDao;
 import springbook.user.sqlservice.jaxb.SqlType;
 import springbook.user.sqlservice.jaxb.Sqlmap;
 
-public class XmlSqlService implements SqlService {
+@SuppressWarnings("restriction")
+public class XmlSqlService implements SqlService, SqlRegistry, SqlReader {
+	private SqlReader sqlReader;
+	private SqlRegistry sqlRegistry;
 	private Map<String, String> sqlMap = new HashMap<String, String>();
 	private String sqlmapFile;
+	
+	public void setSqlmapFile(String sqlmapFile) {
+		this.sqlmapFile = sqlmapFile;
+	}
 
-	@PostConstruct
-	public void loadSql() {
+	public void setSqlReader(SqlReader sqlReader) {
+		this.sqlReader = sqlReader;
+	}
+	
+	public void setSqlRegistry(SqlRegistry sqlRegistry) {
+		this.sqlRegistry = sqlRegistry;
+	}	
+			
+	public void registerSql(String key, String sql) {
+		sqlMap.put(key, sql);
+	}
+	
+	public String findSql(String key) throws SqlNotFoundException {
+		String sql = sqlMap.get(key);
+		if (sql == null) 
+			throw new SqlNotFoundException(key + "에 대한 SQL을 찾을 수 없습니다.");
+		else
+			return sql;
+	}
+
+	public void read(SqlRegistry sqlRegistry) {
 		String contextPath = Sqlmap.class.getPackage().getName();
 		try {
 			JAXBContext context = JAXBContext.newInstance(contextPath);
@@ -26,23 +52,25 @@ public class XmlSqlService implements SqlService {
 			InputStream is = UserDao.class.getResourceAsStream(this.sqlmapFile);
 			Sqlmap sqlmap = (Sqlmap)unmarshaller.unmarshal(is);
 			
-			for (SqlType sql : sqlmap.getSql()) {
-				sqlMap.put(sql.getKey(), sql.getValue());
+			for(SqlType sql : sqlmap.getSql()) {
+				sqlRegistry.registerSql(sql.getKey(), sql.getValue());
 			}
 		} catch (JAXBException e) {
 			throw new RuntimeException(e);
 		}
 	}
-
-	public void setSqlmapFile(String sqlmapFile) {
-		this.sqlmapFile = sqlmapFile;
+	
+	@PostConstruct
+	public void loadSql() {
+		this.sqlReader.read(this.sqlRegistry);
 	}
 	
 	public String getSql(String key) throws SqlRetrievalFailureException {
-		String sql = sqlMap.get(key);
-		if (sql == null)
-			throw new SqlRetrievalFailureException(key + "에 대한 SQL을 찾을 수 없습니다.");
-		else
-			return sql;
+		try {
+			return this.sqlRegistry.findSql(key);
+		}
+		catch(SqlNotFoundException e) {
+			throw new SqlRetrievalFailureException(e);
+		}
 	}	
 }
